55e40f5f71229a2ea882d137ff6102f083e68926
Adalberto777 <albert.tliashev@gmail.com>
2022-12-26 01:11:03 +0500

    task01/2

--- src/main/java/homeWorkTwo/Main.java
+++ src/main/java/homeWorkTwo/Main.java
@@ -1,20 +1,16 @@
 package homeWorkTwo;
 
 
-import static java.lang.StringUTF16.charAt;
-import static java.net.HttpCookie.equalsIgnoreCase;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(checkPalindrome("ararat"));
+        System.out.println(checkPalindrome("arrara"));
     }
 
     private static boolean checkPalindrome(String str) {
         StringBuilder sb = new StringBuilder();
-        for (int i = str.length(); i > 0; i--) {
+        for (int i = str.length() - 1; i > -1; i--) {
             sb.append(str.charAt(i));
         }
-
         if ((str.equalsIgnoreCase(sb.toString()))) return true;
         else return false;
     }
