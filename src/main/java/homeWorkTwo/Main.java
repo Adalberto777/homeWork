f47235033ee37703a0a51eab679b8136bc3fc3a1
Adalberto777 <albert.tliashev@gmail.com>
2022-12-26 22:30:14 +0500

    task02/2

--- src/main/java/homeWorkTwo/Main.java
+++ src/main/java/homeWorkTwo/Main.java
@@ -1,9 +1,34 @@
 package homeWorkTwo;
 
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(checkPalindrome("arrara"));
+    public static void main(String[] args) throws FileNotFoundException {
+//        System.out.println(checkPalindrome("arrara"));
+        getTestInString("TEST", 100);
+        writeToFileTest();
+
+    }
+
+    private static void writeToFileTest() throws FileNotFoundException {
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter("D:\\NewGeekBrains\\Java\\Java_Project\\homeWork\\src\\main\\resources\\sours\\test.txt");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("String is Employ ");
+        }
+        pw.println(getTestInString("TEST", 100));
+        pw.close();
+    }
+
+    private static String getTestInString(String str, Integer count) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++) {
+            sb.append(str);
+        }
+        return sb.toString();
     }
 
     private static boolean checkPalindrome(String str) {
